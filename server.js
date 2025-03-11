0750adb280af857d8731eaa394cc46393fb1e081
cm23abr <charvimaurya0110@gmail.com>
2025-03-11 16:16:55 +0000

    commit

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ const __dirname = path.dirname(__filename);
 //app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(path.join(__dirname, 'public')));
-
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
+
+
 app.use(express.json());
 // Serve the contact form
 app.get('/', (req, res) => {
